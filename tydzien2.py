a3efef1fda735886edc59649086c31aa848a4c87
natalkau <166330083+natalkau@users.noreply.github.com>
2024-04-09 18:57:19 +0200

    Add files via upload

---
+++ tydzien2.py
@@ -0,0 +1,31 @@
+print("Hello World")
+print("x")
+x = [1,5.5,6]
+print(x)
+
+y = [6,1.5,1]
+print(x+y)
+
+a = 15
+b = 135
+print(a+b)
+
+print(a**b)
+print(b//a)
+print(b/a)
+
+print(math.sqrt(a))
+
+z = (a-b)
+print(z/a)
+z = a-b
+print(z)
+
+import math
+
+b=math.sqrt(a)
+print(b)
+
+
+pierwiastek = math.sqrt(a-b)
+print(pierwiastek)
